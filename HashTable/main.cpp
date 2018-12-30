a8478e8dfb04d49349186b9dda30db0a2e5e85f2
asologubova <a.sologubova@g.nsu.ru>
2018-12-30 19:08:47 +0700

    dec30

---
+++ HashTable/main.cpp
@@ -0,0 +1,19 @@
+#include "header.h"
+#include <cstdio>
+#include <string>
+#include <string.h>
+#include <iostream>
+
+int main(int argc, char * argv[]){
+	//bool b;
+	//HashTable a;
+	//Value v1 = { 20, 70 };
+	//a.insert("Vasiliy", v1);
+	//Value v2 = a.at("Vasiliy");
+	////if (v2 == v1) b = 1;
+	//b = a.erase("Vasiliy");
+
+
+
+	return 0;
+}
